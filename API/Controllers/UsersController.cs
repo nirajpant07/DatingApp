656a745646bc8b4b5209133cc68f04e285670cb9
Niraj Pant <Niraj.Pant.C@greenwayhealth.com>
2022-06-29 13:50:39 +0530

    1st Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task< ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
